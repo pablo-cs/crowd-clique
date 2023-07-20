e9b90d707ff2427cdb27a9986b8d720c0480ae9c
Pablo Crisostomo Suarez <pcrisostomosuarez5@gmail.com>
2023-07-20 23:36:20 +0000

    Added events page, connect style.css

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from flask_behind_proxy import FlaskBehindProxy
 
 from app.forms import LoginForm, RegistrationForm
 from flask_sqlalchemy import SQLAlchemy
-
+from app.ticketmaster_api import search_events
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app) 
 app.config['SECRET_KEY'] = '117b3274820db891a19981c6ab2a0fd2'
@@ -32,3 +32,7 @@ def signup():
         return flash(f'Account created for {form.username.data}!', 'success')
         flash(f'Account created for {form.username.data}!', 'success')
     return render_template('signup.html', title='Sign Up', form=form)
+
+def event_landing():
+    events = search_events("concert")
+    return render_template('event_landing.html', events=events)
