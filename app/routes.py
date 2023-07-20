b1808a9eb0431321b47914218a23c468df28c802
Kaothara Morakinyo <kmorakinyo2021@gmail.com>
2023-07-20 21:24:22 +0000

    Kao changes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_behind_proxy import FlaskBehindProxy
 
-from app.forms import RegistrationForm
+from app.forms import LoginForm, RegistrationForm
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -12,14 +12,26 @@ db = SQLAlchemy(app)
 
 def login():
     # Assuming you have a 'templates' folder in your project containing the 'login.html' file
-    from .forms import RegistrationForm
+    from .forms import LoginForm
 def login():
-    form = RegistrationForm()
+    form = LoginForm()
     if form.validate_on_submit(): # checks if entries are valid
-        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        user = User(username=form.username.data,password=form.password.data)
         db.session.add(user)
         db.session.commit()
         return flash(f'Account created for {form.username.data}!', 'success')
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('pat'))
     return render_template('login.html', title='Log In', form=form)
+
+def signup():
+    # Assuming you have a 'templates' folder in your project containing the 'login.html' file
+    from .forms import RegistrationForm
+def signup():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        user = User(username=form.username.data,email=form.email.data,password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return flash(f'Account created for {form.username.data}!', 'success')
+        flash(f'Account created for {form.username.data}!', 'success')
+    return render_template('signup.html', title='Sign Up', form=form)
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_behind_proxy import FlaskBehindProxy
-from app.routes import login
+from app.routes import login,signup
 
 app = Flask(__name__, template_folder='app/templates')
 proxied = FlaskBehindProxy(app)
 app.secret_key ='bae5f8bb14e95a28a1d679fe833a7ba2'
 
-app.route('/')(login)
+app.route('/login')(login)
+app.route('/signup')(signup)
+
+
 
 
 if __name__ == '__main__':
