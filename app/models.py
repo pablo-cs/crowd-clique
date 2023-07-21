c073ebbbbc7779e422bee24624f7084a9d095fab
Emmanuel Obu <eobu003@gmail.com>
2023-07-21 23:59:23 +0000

    added class for comments and replies

--- app/models.py
+++ app/models.py
@@ -15,5 +15,26 @@ class User(db.Model):
   def __repr__(self):
     return f"User('{self.username}', '{self.email}')"
 
-with app.app_context():
-  db.create_all()
+#to store the comments for an event
+class CommentEvent(db.Model):
+    __bind_key__ = 'comment'
+    id = db.Column(db.Integer, primary_key=True)
+    event_id = db.Column(db.Integer, unique=False, nullable = False)
+    event_name = db.Column(db.String(50),unique=False, nullable = False)
+    user_name = db.Column(db.String(20), unique = False, nullable = False)
+    comment = db.Column(db.String(255), unique=False, nullable = False)
+
+    #to check getting right info
+    def __repr__(self):
+        return f"Player('{self.event_id}',{self.event_name},{self.user_name},{self.comment})"
+
+#replies to each comment
+class Replies(db.Model):
+    __bind_key__ = 'comment'
+    id = db.Column(db.Integer, primary_key=True)
+    comment_id = db.Column(db.Integer, unique=False, nullable = False)
+    user_name = db.Column(db.String(20), unique = False, nullable = False)
+    reply = db.Column(db.String(255), unique=False, nullable = False)
+
+
+
