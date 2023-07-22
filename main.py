bd12826d8a23d8dba1b5d2a1aac7c4dbffb675bc
Pablo Crisostomo Suarez <pcrisostomosuarez5@gmail.com>
2023-07-22 06:34:22 +0000

    Removed placeholder method from imports

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_behind_proxy import FlaskBehindProxy
 from app.models import db
-from app.routes import login, signup, home, event_landing, search, err, event_test
+from app.routes import login, signup, home, event_landing, search, err
 
 app = Flask(__name__, static_folder="app/static", template_folder="app/templates")
 proxied = FlaskBehindProxy(app)
