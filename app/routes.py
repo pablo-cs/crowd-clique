9da4b5ec663e63768353dbfb529ee7b788d86f24
Pablo Crisostomo Suarez <pcrisostomosuarez5@gmail.com>
2023-07-22 06:33:34 +0000

    Commented out placeholder functions in routes.py

--- app/routes.py
+++ app/routes.py
@@ -50,20 +50,20 @@ def search():
                 )
     return render_template('search_result.html', search_results=None)
 
-def event_comments():
-    event_id = request.form.get('event_id')
-    event_details = get_event_details(event_id) #gets event object
-    #query the comments database for comments with that event id
-    event_comments = []
-    return render_template('event_comments.html', event_details=event_details,
-    event_comments=event_comments,)
+# def event_comments():
+#     event_id = request.form.get('event_id')
+#     event_details = get_event_details(event_id) #gets event object
+#     #query the comments database for comments with that event id
+#     event_comments = []
+#     return render_template('event_comments.html', event_details=event_details,
+#     event_comments=event_comments,)
 
-def event_replies():
-    event_id = request.form.get('event_id')
-    comment_id = request.form.get('comment_id')
-    #query database for comment with that comment id
-    event_details = get_event_details(event_id)
+# def event_replies():
+#     event_id = request.form.get('event_id')
+#     comment_id = request.form.get('comment_id')
+#     #query database for comment with that comment id
+#     event_details = get_event_details(event_id)
     
-    #query database for replies with that comment id
-    return render_template('event_replies.html', event_details=event_details, comment=comment
-    comment_replies=comment_replies)
+#     #query database for replies with that comment id
+#     return render_template('event_replies.html', event_details=event_details, comment=comment
+#     comment_replies=comment_replies)
