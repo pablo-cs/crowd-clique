da7e7a41d2bd3909506f4ba399850cb2bb43fc10
Kaothara Morakinyo <kmorakinyo2021@gmail.com>
2023-07-23 22:30:46 +0000

    Kao new changes

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for,flash
+from flask import Flask, render_template, request, redirect, url_for,flash, session
 from flask_behind_proxy import FlaskBehindProxy
 from app.models import User,db
 from app.forms import LoginForm, RegistrationForm
 from flask_sqlalchemy import SQLAlchemy
 from app.ticketmaster_api import search_events, suggest_events
 
-img = {'d': '../static/img/dog.jpg', 'c': '../static/img/cat.jpg','s': '../static/img/sunset.jpg'}
+img = {'d': '/img/dog.jpg', 'c': '/img/cat.jpg','s': '/img/sunset.jpg'}
 
 
 def home():
@@ -20,12 +20,18 @@ def login():
         user=User.query.filter_by(username=form.username.data).first()
         if user:
             flash(f'Welcome,{form.username.data}!')
+            session['username'] = user.username
             return redirect(url_for('event_landing'))
         else:
             return redirect(url_for('err'))
+
     return render_template('login.html', title='Log In', form=form)
 
-
+##@app.route('/logout')
+def logout():
+   # remove the username from the session if it is there
+   session.pop('username', None)
+   return redirect(url_for('index'))
 
 def signup():
     form = RegistrationForm()
@@ -33,12 +39,14 @@ def signup():
         user = User(name=form.name.data,username=form.username.data,email=form.email.data,password=form.password.data,pronouns=form.pronouns.data, avatar=img[form.avatar.data])
         db.session.add(user)
         db.session.commit()
+        session['username'] = user.username
         return redirect(url_for('event_landing'))
     return render_template('signup.html', title='Sign Up', form=form)
 
 def event_landing():
     events = suggest_events()
-    return render_template('event_landing.html', your_events=[], suggested_events=events)
+    user = User.query.filter_by(username=session['username']).first()
+    return render_template('event_landing.html', your_events=[], suggested_events=events, user=user)
 
 def search():
     search_query = request.form.get('search')
