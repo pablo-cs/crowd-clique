790a9be973cdaa43622b25c48188ba64cb86b540
Pablo Crisostomo Suarez <pcrisostomosuarez5@gmail.com>
2023-07-24 04:12:26 +0000

    Implementing commenting and replies into event_comments and event_replies.html

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for,flash
+from flask import Flask, render_template, request, redirect, url_for,flash, session
 from flask_behind_proxy import FlaskBehindProxy
-from app.models import User,db
-from app.forms import LoginForm, RegistrationForm
+from app.models import User,db, CommentEvent, Reply
+from app.forms import LoginForm, RegistrationForm, CommentForm
 from flask_sqlalchemy import SQLAlchemy
-from app.ticketmaster_api import search_events, suggest_events
+from app.ticketmaster_api import search_events, suggest_events, get_event_details
+from datetime import datetime
 
 img = {'d': '../static/img/dog.jpg', 'c': '../static/img/cat.jpg','s': '../static/img/sunset.jpg'}
 
@@ -20,6 +21,7 @@ def login():
         user=User.query.filter_by(username=form.username.data).first()
         if user:
             flash(f'Welcome,{form.username.data}!')
+            session['username'] = form.username.data
             return redirect(url_for('event_landing'))
         else:
             return redirect(url_for('err'))
@@ -31,6 +33,7 @@ def signup():
     form = RegistrationForm()
     if form.validate_on_submit(): # checks if entries are valid
         user = User(name=form.name.data,username=form.username.data,email=form.email.data,password=form.password.data,pronouns=form.pronouns.data, avatar=img[form.avatar.data])
+        session['user_name'] = form.username.data
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('event_landing'))
@@ -52,25 +55,27 @@ def search():
 
 
 def add_comment():
-    user_name = request.form.get('user_name')
+    user_name = session.get('user_name')
     comment = request.form.get('user_comment')
     event_id = request.form.get('event_id')
-    player_data = get_player_data(player_name)
-    comment = CommentPlayer(event_id=event_id, user_name=user_name, comment=comment)
+    event_details = get_event_details(event_id)
+    current_time = datetime.now()
+    comment = CommentEvent(event_id=event_id, user_name=user_name, comment=comment, timestamp=current_time)
     db.session.add(comment)
     db.session.commit()
     form = CommentForm()
-    event_comments = CommentPlayer.query.filter_by(event_id=event_id).all()
+    event_comments = CommentEvent.query.filter_by(event_id=event_id).all()
     return render_template(
-        'event.html', 
+        'event_comments.html',event_details=event_details,
         comments=event_comments,
         form=form
     )
 
 def add_reply():
-    user_name = request.form.get('user_name')
-    reply = request.form.get('user_comment')
+    user_name = session.get('user_name')
+    reply = request.form.get('reply')
     event_id = request.form.get('event_id')
+    event_details = get_event_details(event_id)
     comment_id = request.form.get('comment_id')
     #how are we getting comment id
     reply= Reply(comment_id=comment_id,event_id=event_id, user_name=user_name, reply=reply)
@@ -79,7 +84,7 @@ def add_reply():
     form = CommentForm()
     comment_replies = Reply.query.filter_by(comment_id=comment_id).all()
     return render_template(
-        'event_replies.html', 
+        'event_replies.html', event_details=event_details, comment_id=comment_id,
         comment_replies=event_replies,
         form=form
     )
@@ -90,8 +95,9 @@ def event_comments():
     event_details = get_event_details(event_id) #gets event object
     #query the comments database for comments with that event id
     event_comments = CommentEvent.query.filter_by(event_id=event_id).all()
+    form = CommentForm()
     return render_template('event_comments.html', event_details=event_details,
-    event_comments=event_comments,)
+    event_comments=event_comments,form=form)
 
 def event_replies():
     event_id = request.form.get('event_id')
@@ -99,5 +105,6 @@ def event_replies():
     event_details = get_event_details(event_id)
     #query database for replies with that comment id
     comment_replies = Reply.query.filter_by(comment_id=comment_id).all()
-    return render_template('event_replies.html', event_details=event_details,comment=comment
-    comment_replies=comment_replies)
+    form = CommentForm()
+    return render_template('event_replies.html', event_details=event_details,comment_id=comment_id,
+    replies=comment_replies, form=form)
